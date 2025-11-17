a72f73e61abbbf15fa3eb1c8499d1b25f52c05b7
rrrnnn986954 <negima6954@gmail.com>
2025-11-17 13:53:08 +0900

    tweets_controller.rbを編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -40,5 +40,11 @@ class TweetsController < ApplicationController
   def set_tweet
     @tweet = Tweet.find(params[:id])
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
   
 end
